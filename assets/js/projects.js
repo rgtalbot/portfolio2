2c4189bbd60bbb2fee3c7d42ff29b27fc8307636
rgtalbot <rgtalbot@users.noreply.github.com>
2016-09-21 15:50:50 -0400

    project modal started

---
+++ assets/js/projects.js
@@ -0,0 +1,20 @@
+var projects = {
+    "hangman": {
+        "headerTop": "BOOTCAMP PROJECT",
+        "title": "80s HANGMAN",
+        "role": "ROLE: LEAD DESIGNER",
+        "pageContent": [
+              "This was one of my first projects at UCF Coding Bootcamp. The assignment was to build a hangman game using javascript that allows a player to guess letters by pressing the keys on the keyboard . When I initally built the project, it was mostly Vanilla JS. The original project did have the music play when you won and the picture change."
+            ,
+           "Since completing it for the boot camp, I have gone back and added letter buttons so that a player can play on a mobile device. I have also reformatted the code to mostly jQuery and cleaned it up for readability."
+            ,
+
+               "Feel free to play the game and let me know what you think of it!"
+
+        ],
+        "pageLink": "https://eightieshangman.herokuapp.com/",
+        "gitLink": "https://github.com/rgtalbot/week-3-game",
+        "images": ["assets/img/portfolio/hangman1.png"]
+    }
+
+}
